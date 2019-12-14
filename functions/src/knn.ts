230711780bcb61e588e8c517a492685b778d0bd9
Ali-Amin <ali.m.amin98@gmail.com>
2019-12-14 21:07:16 +0200

    Infer types

--- functions/src/knn.ts
+++ functions/src/knn.ts
@@ -35,6 +35,6 @@ export function knn(songs: Song[], userSong: Song): Song[] {
     return a.distance - b.distance;
   });
 
-  let temp = distanceSongPairs.slice(0, 10).map((pair: Pair) => pair.song);
+  const temp: Song[] = distanceSongPairs.slice(0, 10).map((pair: Pair) => pair.song);
   return temp;
 }
